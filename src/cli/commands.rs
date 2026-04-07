aac5cb011a7e15423a23aa2a472e33e2f7adfb37
qing.liu <qing.liu@iglooinsure.com>
2026-04-07 16:04:11 +0800

    chore: auto commit after Claude edit

--- src/cli/commands.rs
+++ src/cli/commands.rs
@@ -3,7 +3,7 @@ use anyhow::Result;
 use chrono::{DateTime, Utc};
 use std::process::Command;
 
-use crate::core::{repository::Repository, sync::SyncEngine};
+use crate::core::{repository::Repository, sync::SyncEngine, project::ProjectManager};
 use crate::utils::{fs, ui};
 
 #[derive(Parser)]
